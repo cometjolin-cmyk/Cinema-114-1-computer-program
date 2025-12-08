04c4dd0d95fbe9c07100ebafdb236492aa358380
許家羚 <comet@xujialingdeMacBook-Air.local>
2025-12-08 23:10:08 +0800

    購票：修正 5 處錯誤並整理流程

--- my.py
+++ my.py
@@ -38,6 +38,7 @@ def purchase_flow(): #自訂函式＿購買程序：列出場次並購票
         "movie": "Horror Night",
         "time": "19:00",
         "price": 250,
+        "age": 15,
         "seats": {
             "A1": True,
             "A2": True,
@@ -51,6 +52,7 @@ def purchase_flow(): #自訂函式＿購買程序：列出場次並購票
         "movie": "Love in Taipei",
         "time": "21:00",
         "price": 220,
+        "age": 18,
         "seats": {
             "A1": True,
             "A2": True,
@@ -62,17 +64,37 @@ def purchase_flow(): #自訂函式＿購買程序：列出場次並購票
     return Movie_time_list #函式內的變數不會自動傳回到外面，故需要回傳 #不建議用gobal，因為函式依賴外部狀態，修改全域變數可能影響程式其他地方，容易出錯。
 Movie_time_list = purchase_flow() #是把函式回傳的資料「拿出來」存成變數。
 for movie in Movie_time_list: #設變數movie為卡片（Movie_time_list裡，有幾個就執行幾次迴圈）
-    print(f"編號: {movie['id']}, 電影: {movie['movie']}, 時間: {movie['time']}, 價格: {movie['price']}") #因為Movie_time_list 串列，電影一的資料是用字典故可以取出用f{“key"}
+    print(f"編號: {movie['id']}, 電影: {movie['movie']}, 時間: {movie['time']}, 價格: {movie['price']}, 限制年齡: {movie[]}") #因為Movie_time_list 串列，電影一的資料是用字典故可以取出用f{“key"}
 
 #請使用者輸入電影編號，輸出可以選得座位
-movie_number = int(input("購票系統＿請輸入想購買的電影編號"))
+def choose_seats():
+    movie_number = int(input("購票系統＿請輸入想購買的電影編號"))
 
-for seat, available in movie['seats'].items():
-    status = "可選"if available == True else "已售出"
-    print(f"  {seat}: {status}")
-    
+    for seat, available in movie['seats'].items(): #為什麼我可以movie['seats']這樣去指定
+        status = "可選"if available == True else "已售出"
+        print(f"  {seat}: {status}")
 
-    
+    buy_seat = input("請輸入要購買的位子").strip()
+    if buy_seat not in movie['seats']:
+        print("無此座位喔")
+    elif available == False :
+        print("此座位已售出")
+    else :
+        print("座位可購買，請填寫資料")
+
+    today = datetime.date.today()
+    thisYear = today.year
+    ageYear = int(input("請輸入出生年齡，以確保可以購買此場電影"))
+    age = thisYear - ageYear #記得轉格式 字串不可運算
+   
+    if age >= :
+        print(f"您目前{age}歲可以購買18+限制級票\n")
+        buy = bool(input("是否購票(請輸入true或false) ")
+        if buy = true:
+            identity = bool(input("是否有其他優惠身份(請輸入true或false) ")
+            
+
+        
 
 
 
